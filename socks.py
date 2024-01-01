2981a6511e726fed1074aa41b8e4f53d96b9c1b2
Dexter-Hysteria <145413662+Dexter-Hysteria@users.noreply.github.com>
2024-01-01 16:47:21 +0800

    Update socks.py

--- socks.py
+++ socks.py
@@ -172,7 +172,7 @@ class ConnectionHandler(threading.Thread):
             if self.method=='CONNECT':
                 port = 443
             else:
-                port = 22
+                port = sys.argv[1]
 
         (soc_family, soc_type, proto, _, address) = socket.getaddrinfo(host, port)[0]
 
